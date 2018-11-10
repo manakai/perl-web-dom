c389356f310a49b8a0d4aa48dddcdcb272beb31b
gaa <gaa@gaa3.h>
2018-11-10 18:45:30 +0000

    auto

--- lib/Web/DOM/_Defs.pm
+++ lib/Web/DOM/_Defs.pm
@@ -1,21 +1,21 @@
 $Web::DOM::_Defs = {
           'all_named' => {
                            'http://www.w3.org/1999/xhtml' => {
-                                                             'a' => 1,
-                                                             'button' => 1,
-                                                             'embed' => 1,
-                                                             'form' => 1,
-                                                             'frame' => 1,
-                                                             'frameset' => 1,
-                                                             'iframe' => 1,
-                                                             'img' => 1,
-                                                             'input' => 1,
-                                                             'map' => 1,
-                                                             'meta' => 1,
-                                                             'object' => 1,
-                                                             'select' => 1,
-                                                             'textarea' => 1
-                                                           }
+                                                               'a' => 1,
+                                                               'button' => 1,
+                                                               'embed' => 1,
+                                                               'form' => 1,
+                                                               'frame' => 1,
+                                                               'frameset' => 1,
+                                                               'iframe' => 1,
+                                                               'img' => 1,
+                                                               'input' => 1,
+                                                               'map' => 1,
+                                                               'meta' => 1,
+                                                               'object' => 1,
+                                                               'select' => 1,
+                                                               'textarea' => 1
+                                                             }
                          },
           'legacy_event' => {
                               'animationend' => 'webkitAnimationEnd',
